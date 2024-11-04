febeca10fc886de668f22076c8dc945ea8fc2e3b
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-04 11:47:33 +0100

    Ajout de bien.cs et maison.cs, modification de terrain.cs pour hérité de Bien.cs

--- Terrain.cs
+++ Terrain.cs
@@ -3,48 +3,42 @@ using System.Text.RegularExpressions;
 
 namespace ExemplePOO
 {
-    public class Terrain
+    public class Terrain : Bien
     {
-        public string Adresse;
-        public float Superficie;
-        public int NbCoteCloture;
-        public bool Rivière;
+        public int NbCotesClotures;
+        public bool Riviere;
 
-        public Terrain(string adresse, float superficie, int nbCoteCloture, bool rivière)
+        public Terrain(string adresse, float superficie, int nbCotesClotures, bool riviere) : base(adresse, superficie)
         {
-            Adresse = adresse;
-            Superficie = superficie;
-            NbCoteCloture = nbCoteCloture;
-            Rivière = rivière;
+            NbCotesClotures = nbCotesClotures;
+            Riviere = riviere;
         }
 
         public override string ToString()
         {
-            string toString = String.Format("Adresse = {0}\n", this.Adresse);
-            toString += String.Format("Superficie = {0}m²\n", this.Superficie);
-            toString += String.Format("Nombre de coté de cloture = {0}\n", this.NbCoteCloture);
-            toString += String.Format("Présence d'un Rivière = {0}\n", this.Rivière ? "Oui" : "Non");
-            toString += String.Format("> VALEUR = {0}$", this.EvaluationValeur());
-            toString += String.Format("\n> Cout pour finir la cloture = {0}$", this.CoutFinirCloture());
+            string toString = String.Format("Nombre de cotés clorutés = {0}\n", this.NbCotesClotures);
+            toString += String.Format("Présence d'une rivière = {0}\n", this.Riviere ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$\n", this.EvaluationValeur());
             return toString;
         }
 
-        public float EvaluationValeur()
+        public new float EvaluationValeur()
         {
             int facteur = 3000;
 
-            if (this.Rivière) { facteur += 500; }
-            if (this.NbCoteCloture > 3) { facteur += 200; }
+            if (this.Riviere) { facteur += 600; }
+
+            facteur += 100 * this.NbCotesClotures;
 
             if (Regex.IsMatch(this.Adresse, @"\bParis\b")) { facteur += 7000; }
             else if (Regex.IsMatch(this.Adresse, @"\bLyon\b")) { facteur += 2000; }
 
             return this.Superficie * facteur;
         }
-
+ 
         public int CoutFinirCloture()
         {
-            return (int)(this.Superficie * this.NbCoteCloture);
+            return (int)(this.Superficie * this.NbCotesClotures);
         }
     }
 }
