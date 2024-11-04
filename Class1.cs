4daacf60a29381247745147954165d291394d840
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-04 10:03:53 +0100

    Ajoutez des fichiers projet.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExemplePOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Terrain UneTerrain = new Terrain("11 Rue des Chartreux, 69001 Lyon", 58f, 4, false);
+            Terrain UneAutreTerrain = new Terrain("4 place Saint Louis, 22100 Dinan", 86.5f, 5, true);
+            Terrain UneDerniereTerrain = new Terrain("26 Boulevard Claude Lorrin, 40100 Dax", 25.2f, 2, false);
+
+            Terrain[] CatalogueTerrains = new Terrain[] { UneTerrain, UneAutreTerrain, UneDerniereTerrain };
+
+            foreach (Terrain T in CatalogueTerrains)
+            {
+                Console.WriteLine(T);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
