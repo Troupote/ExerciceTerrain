e6fb9f266057d2478750cfe5eab904c992191620
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-04 13:18:17 +0100

    ajout de propriétaire.cs

--- Program.cs
+++ Program.cs
@@ -13,12 +13,16 @@ namespace Uml1Terrain
             Terrain UnTerrain = new Terrain("55 route cabossée, 29130 Locmaria-Plouzané", 5000f, 2, true);
             Terrain UnAutreTerrain = new Terrain("102 route des volcans, 63000 Clermont-Ferrand", 1500f, 4, false);
 
-            Bien[] CatalogueBiens = new Bien[] { UneMaison, UneAutreMaison, UneDerniereMaison, UnTerrain, UnAutreTerrain };
+            List<Bien> UnListDeBiens = new List<Bien> { UneMaison, UneAutreMaison, UneDerniereMaison, UnTerrain, UnAutreTerrain };
 
-            foreach (Bien B in CatalogueBiens)
+            Proprietaire UnProprietaire = new Proprietaire("Dupont", "Jean", UnListDeBiens);
+
+            Console.WriteLine(@"Propriétaire : {0} {1}", UnProprietaire.Prenom, UnProprietaire.Nom);
+            foreach (Bien B in UnProprietaire.Biens)
             {
                 Console.WriteLine(B);
             }
         }
     }
 }
+ 
---
+++ Proprietaire.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uml1Terrain
+{
+    public class Proprietaire
+    {
+        public string Nom;
+        public string Prenom;
+        public List<Bien> Biens;
+
+        public Proprietaire(string nom, string prenom, List<Bien> ListBiens)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Biens = ListBiens;
+        }
+
+        public override string ToString()
+        {
+            string toString = String.Format("Nom = {0}\n", this.Nom);
+            toString += String.Format("Prénom = {0}\n", this.Prenom);
+            toString += "Biens :\n";
+            foreach (Bien bien in this.Biens)
+            {
+                toString += bien.ToString();
+            }
+            return toString;
+        }
+
+        }
+}
